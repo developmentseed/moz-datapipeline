403f1cd7e75f69ca9b9edd037eb91e736d38fb43
Daniel da Silva <daniel@developmentseed.org>
2018-02-08 16:53:02 +0000

    Add script to merge indicators into road network

---
+++ scripts/utils/utils.js
@@ -0,0 +1,11 @@
+'use strict';
+const Promise = require('bluebird');
+
+export function tap (fn) {
+  return (data) => {
+    const res = fn();
+    return res instanceof Promise
+      ? res.then(() => data)
+      : Promise.resolve(data);
+  };
+}
