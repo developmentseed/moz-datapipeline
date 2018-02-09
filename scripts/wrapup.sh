df47fe0c368ee9c19b58e33b57e80da66c2058c4
Olaf Veerman <olaf.veerman@gmail.com>
2018-02-09 11:34:06 -0500

    Add district boundaries These will be used for calculation of poverty and fishery indicator

--- scripts/wrapup.sh
+++ scripts/wrapup.sh
@@ -4,4 +4,5 @@ TMP_DIR=./.tmp
 
 # Temporary. Will ultimately handle stitching the base road network + indicators together and the conversion to Vector Tiles.
 cp $TMP_DIR/roadnetwork* ./output
-cp $TMP_DIR/boundaries* ./output
+cp $TMP_DIR/prov_boundaries* ./output
+cp $TMP_DIR/district_boundaries.geojson ./output
