e74e3479e6df42890816e9d1b86912eceb256694
Olaf Veerman <olaf.veerman@gmail.com>
2018-02-15 15:56:42 -0500

    Call correct poverty script

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 bash ./scripts/preparation.sh
 
-node ./scripts/indicator-from-areas.js .tmp/district_boundaries.geojson POV_HCR poverty
+node ./scripts/indicator-from-areas/index.js .tmp/district_boundaries.geojson POV_HCR poverty
 
 bash ./scripts/wrapup.sh
