8519e2ec3b2dffa3031a5a8898af9e28e479b7de
Matthew Hanson <matt.a.hanson@gmail.com>
2018-02-18 22:51:30 -0500

    rearrange, save stats, upload, use cached stats if available

--- scripts/img-stats.py
+++ scripts/img-stats.py
@@ -25,31 +25,6 @@ def uri_parser(uri):
     }
 
 
-def mkdirp(path):
-    """ Recursively make directory """
-    if not os.path.isdir(path):
-        os.makedirs(path)
-    return path
-
-
-def copy_files(filenames, path='/tmp'):
-    """ Copy files from s3 to local storage """
-    fnames = filenames
-    s3 = boto3.client('s3')
-    fnames = []
-    for f in filenames:
-        if f[0:5] != 's3://':
-            continue
-        uri = uri_parser(f)
-        fout = os.path.join(path, uri['key'])
-        mkdirp(os.path.dirname(fout))
-        if not os.path.exists(fout):
-            print('downloading %s' % f)
-            s3.download_file(uri['bucket'], uri['key'], fout)
-        fnames.append(fout)
-    return fnames
-
-
 def s3_list(uri):
     """ Get list of objects within bucket and path """
     s3 = boto3.client('s3')
@@ -61,6 +36,24 @@ def s3_list(uri):
         for file in response['Contents']:
             filenames.append(os.path.join('s3://%s' % s3_uri['bucket'], file['Key']))
     return filenames
+
+
+def upload(filename, uri, extra={}):
+    """ Upload object to S3 uri (bucket + prefix), keeping same base filename """
+    s3 = boto3.client('s3')
+    s3_uri = uri_parser(uri)
+    bname = os.path.basename(filename)
+    uri_out = 's3://%s' % os.path.join(s3_uri['bucket'], os.path.join(s3_uri['key'], bname))
+    with open(filename, 'rb') as data:
+        s3.upload_fileobj(data, s3_uri['bucket'], os.path.join(s3_uri['key'], bname), ExtraArgs=extra)
+    return uri_out
+
+
+def mkdirp(path):
+    """ Recursively make directory """
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    return path
 
 
 def convert_coordinates(coords, origin, dst):
@@ -103,28 +96,53 @@ def get_stats(filenames, geom):
     return stats
 
 
-def find_files(inputdir, path='./'):
+def find_files(inputdir, ext='tif', path='./'):
     if inputdir[0:5] == 's3://':
-        filenames = [f for f in s3_list(inputdir) if os.path.splitext(f)[1] == '.tif']
+        filenames = [f for f in s3_list(inputdir) if os.path.splitext(f)[1] == '.%s' % ext]
     else:
-        filenames = glob.glob(os.path.join(inputdir, '*.tif'))
+        filenames = glob.glob(os.path.join(inputdir, '*.%s' % ext))
     return filenames
 
 
+def copy_files(filenames, path='/tmp'):
+    """ Copy files from s3 to local storage """
+    fnames = filenames
+    s3 = boto3.client('s3')
+    fnames = []
+    for f in filenames:
+        if f[0:5] != 's3://':
+            continue
+        uri = uri_parser(f)
+        fout = os.path.join(path, uri['filename'])
+        mkdirp(os.path.dirname(fout))
+        if not os.path.exists(fout):
+            print('downloading %s' % f)
+            s3.download_file(uri['bucket'], uri['key'], fout)
+        fnames.append(fout)
+    return fnames
+
+
 def main(inputdir, aoi, path):
-    filenames = find_files(inputdir)
-    filenames = copy_files(filenames, path=path)
-    with open(aoi, 'r') as f:
-        gj = json.loads(f.read())
-    header = 'RoadID, max'
-    print(header)
-    for feat in gj['features']:
-        if feat['geometry'] is not None:
-            stats = get_stats(filenames, feat['geometry'])
-            maxval = max([s['max'] for s in stats])
-            #if maxval == 999 or maxval == -9999:
-            #    import pdb; pdb.set_trace()
-            print('%s, %s' % (feat['properties']['NAME'], maxval))
+    filenames = copy_files(find_files(inputdir), path=path)
+    copy_files(find_files(inputdir, ext='csv'), path=path)
+    fout = os.path.join(path, os.path.splitext(os.path.basename(aoi))[0] + '.csv')
+    if not os.path.exists(fout):
+        # calculate stats
+        with open(str(aoi), 'r') as f:
+            gj = json.loads(f.read())
+        f = open(fout, 'w')
+        f.write('RoadID, max' + '\n')
+        print(os.path.join(path, fout))
+        for feat in gj['features']:
+            if feat['geometry'] is not None:
+                stats = get_stats(filenames, feat['geometry'])
+                maxval = max([s['max'] for s in stats])
+                #if maxval == 999 or maxval == -9999:
+                #    import pdb; pdb.set_trace()
+                f.write('%s,%s\n' % (feat['properties']['NAME'], maxval))
+        f.close()
+        # upload to s3
+        upload(fout, inputdir)
 
 
 def parse_args(args):
@@ -132,7 +150,7 @@ def parse_args(args):
     parser = argparse.ArgumentParser(description='Remote image stats', formatter_class=dhf)
     parser.add_argument('inputdir', help='Directory containing files (or S3 Bucket/prefix)')
     parser.add_argument('--aoi', help='GeoJSON filename')
-    parser.add_argument('--path', help='Local path to save files')
+    parser.add_argument('--path', help='Local path to save files', default='./')
     return vars(parser.parse_args(args))
 
 
