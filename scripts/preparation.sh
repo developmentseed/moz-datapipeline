2ec6f38d5034a229380321dd20994801e4b02acb
Olaf Veerman <olaf.veerman@gmail.com>
2018-04-18 08:34:04 -0400

    Remove obsolete props from road data

--- scripts/preparation.sh
+++ scripts/preparation.sh
@@ -65,16 +65,10 @@ mkdir $TMP_DIR
 #       NAME (String) - unique id of the road segment. Example: R850-T2150
 #       ROAD_NAME (String) - name of the road. Example: Combomune -- Macandze
 #       ROAD_ID (String) - id of the road the segment belongs to. Example: R850
-#       START_LOC (String) - Example: Fr. Mabalane
-#       STA_POINT (String) - Example: Fr. Mabalane
-#       END_LOC (String) - Example: Dindiza
-#       END_POINT (String) - Example: Crz. R850/R441
 #       ROAD_CLASS (String) - Example: Vicinal
 #       SURF_TYPE (String) - Example: Unpaved
 #       PAVE_WIDTH (String) - Example: 3.5m
 #       AVG_COND (String) - Example: Fair
-#       DISTRICT (String) - Example: Chigubo
-#       PROVINCE (String) - Example: Gaza
 #       AADT (Real) - average annual daily traffic Example: 70.000000
 #       RUC (Real) - Road User Cost per kilometer. Example: 0.112476
 #   - remove features that have no geometry
@@ -89,7 +83,7 @@ ogr2ogr $TMP_DIR/roadnetwork.shp "$RN_FILE" \
 
 ogr2ogr -f "GeoJSON" $TMP_DIR/roadnetwork.geojson $TMP_DIR/roadnetwork.shp \
   -dialect sqlite \
-  -sql "SELECT NAME, ROAD_NAME, ROAD_ID, START_LOC, STA_POINT, END_LOC, END_POINT, ROAD_CLASS, SURF_TYPE, PAVE_WIDTH, AVG_COND, DISTRICT, PROVINCE, AADT, RUC, geometry \
+  -sql "SELECT NAME, ROAD_NAME, ROAD_ID, ROAD_CLASS, SURF_TYPE, PAVE_WIDTH, AVG_COND, AADT, RUC, geometry \
     FROM roadnetwork \
     WHERE geometry is not null" \
   -nln roadnetwork
