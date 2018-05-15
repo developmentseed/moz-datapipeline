a6f40017afabf002296395c7c94c111d969c796d
Daniel da Silva <daniel@developmentseed.org>
2018-05-15 15:51:49 +0100

    Include code for debugger attachment

--- scripts/eaul/index.js
+++ scripts/eaul/index.js
@@ -1,7 +1,8 @@
-// only ES5 is allowed in this file
 require('babel-register')({
   presets: [ 'es2015' ],
-  plugins: [ 'transform-regenerator', 'syntax-async-functions' ]
+  plugins: [ 'transform-regenerator', 'syntax-async-functions' ],
+  'sourceMaps': 'inline',
+  'retainLines': true
 });
 
 require('babel-polyfill');
